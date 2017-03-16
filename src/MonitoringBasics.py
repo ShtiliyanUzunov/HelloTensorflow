30ecd212bff6ca234c7a98ad6a09bae9e0d456e4
keremidko1990@abv.bg <keremidko1990@abv.bg>
2017-03-16 22:05:27 +0200

    MandelbrotSet example addedid

--- src/MonitoringBasics.py
+++ src/MonitoringBasics.py
@@ -5,6 +5,8 @@ from __future__ import print_function
 import tensorflow as tf
 import numpy as np
 
+tf.logging.set_verbosity(tf.logging.INFO)
+
 # Data sets
 IRIS_TRAINING = "Iris_data/iris_training.csv"
 IRIS_TEST = "Iris_data/iris_test.csv"
@@ -22,13 +24,18 @@ test_set = tf.contrib.learn.datasets.base.load_csv_with_header(
 # Specify that all features have real-value data
 feature_columns = [tf.contrib.layers.real_valued_column("", dimension=4)]
 
+validation_monitor = tf.contrib.learn.monitors.ValidationMonitor(
+    test_set.data,
+    test_set.target,
+    every_n_steps=50)
+
 # Build 3 layer DNN with 10, 20, 10 units respectively.
 classifier = tf.contrib.learn.DNNClassifier(feature_columns=feature_columns,
                                             hidden_units=[10, 20, 10],
                                             n_classes=3,
                                             model_dir="/tmp/iris_model")
 
-classifier.fit(x=training_set.data, y=training_set.target, steps=2000)
+classifier.fit(x=training_set.data, y=training_set.target, steps=2000, monitors=[validation_monitor])
 
 accuracy_score = classifier.evaluate(x=test_set.data, y=test_set.target)["accuracy"]
 print('Accuracy: {0:f}'.format(accuracy_score))
